c3fb70460417dd8b8fb9a79e18047e184ac71861
Laboratorio-Info <ketelinoliveira23@gmail.com>
2022-08-30 22:12:27 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/my/AppViewModel.java
@@ -0,0 +1,7 @@
+package br.com.etecia.my;
+
+import androidx.lifecycle.ViewModel;
+
+public class AppViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/br/com/etecia/my/appFragment.java
@@ -0,0 +1,36 @@
+package br.com.etecia.my;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class appFragment extends Fragment {
+
+    private AppViewModel mViewModel;
+
+    public static appFragment newInstance() {
+        return new appFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_app, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(AppViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
